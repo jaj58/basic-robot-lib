104225120bca9e3b93ce4768da3c14a5d10bd6f3
jaj58 <jacjoneslfc@gmail.com>
2017-11-13 21:41:31 +0000

    explained code better...
    ...

--- example/example.ino
+++ example/example.ino
@@ -1,6 +1,12 @@
 #include <basic-robot-lib.hpp>
 
-c_robot robot(13,11,12,3,1200);
+byte left_dir_pin = 13;
+byte left_speed_pin = 11;
+byte right_dir_pin = 12;
+byte right_speed_pin = 3;
+const float time_taken_one_rot_ms = 1200
+
+c_robot robot(left_dir_pin, left_speed_pin, right_dir_pin, right_speed_pin, time_taken_one_rot_ms);
 void setup() {
   // put your setup code here, to run once:
   robot.setup();
